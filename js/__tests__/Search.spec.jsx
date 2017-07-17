5ee149dce6f6a7e7a41203101b10fbf53a6407f2
Gustavo <andrescabral.c@gmail.com>
2017-07-17 04:12:09 -0400

    test: Search component test added

---
+++ js/__tests__/Search.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Search from '../Search'
+
+test('Search renders correctly', () => {
+  const component = shallow(<Search />)
+  expect(component).toMatchSnapshot()
+})
