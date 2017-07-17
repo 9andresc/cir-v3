4df3907a9833734d07ceaa1633ec3323d3f0ae6b
Gustavo <andrescabral.c@gmail.com>
2017-07-16 22:08:09 -0400

    refactor(index.html): index.html now uses the js bundle from webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+const MyTitle = props =>
+  ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 
-const MyFirstComponent = function() {
-  return ce(
+const MyFirstComponent = () =>
+  ce(
     'div',
     { id: 'my-first-component' },
     ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
     ce(MyTitle, { title: 'Breaking Bad', color: 'GreenYellow' }),
     ce(MyTitle, { title: 'Orange is the New Black', color: 'LimeGreen' })
   );
-};
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'));
