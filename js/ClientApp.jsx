56521ec42202e4df3988337200a54e8ca9014ece
Gustavo <andrescabral.c@gmail.com>
2017-07-16 23:36:26 -0400

    refactor: ClientApp renew with a simple application code

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const MyTitle = props => {
-  const style = { color: props.color }
-
-  return (
-    <div>
-      <h1 style={style}>{props.title}</h1>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
     </div>
-  )
-}
-
-const MyFirstComponent = () => (
-  <div id="my-first-component">
-    <MyTitle title="Game of Thrones" color="YellowGreen" />
-    <MyTitle title="Breaking Bad" color="GreenYellow" />
-    <MyTitle title="Orange is the New Black" color="peru" />
-    <MyTitle title="Rick and Morty" color="burlywood" />
   </div>
 )
 
-render(<MyFirstComponent />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
