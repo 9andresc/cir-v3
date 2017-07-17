0a1cd3577f049c30c2e0d845b240d58b9797b89a
Gustavo <andrescabral.c@gmail.com>
2017-07-17 04:33:50 -0400

    test(Test for correct amount of shows based on search term added):

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -15,7 +15,17 @@ test('Search should render correct amount of shows', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
-xtest(
-  'Search should render correct amount of shows based on search term',
-  () => {}
-)
+test('Search should render correct amount of shows based on search term', () => {
+  const component = shallow(<Search />)
+  const searchWord = 'black'
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length
+
+  component.find('input').simulate('change', { target: { value: searchWord } })
+
+  expect(component.find(ShowCard).length).toEqual(showCount)
+})
