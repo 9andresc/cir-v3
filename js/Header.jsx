75a7228b94160fc6cf8ed048cce36fd7121aba6d
Gustavo <andrescabral.c@gmail.com>
2017-07-17 09:44:06 -0400

    feat: Header component added

---
+++ js/Header.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const Header = () => (
+  <header>
+    <h1><Link to="/">svideo</Link></h1>
+  </header>
+)
+
+export default Header
