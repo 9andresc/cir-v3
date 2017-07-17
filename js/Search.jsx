5dba6ce4451d63086ded46e9a73e2defa6314bd7
Gustavo <andrescabral.c@gmail.com>
2017-07-17 01:39:41 -0400

    refactor: key property added on every ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -6,7 +6,7 @@ import ShowCard from './ShowCard'
 
 const Search = () => (
   <div className="search">
-    {preload.shows.map(show => <ShowCard show={show} />)}
+    {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
   </div>
 )
 
