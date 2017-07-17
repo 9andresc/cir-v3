1581fb95df176572b18549c954195db9a1ff2af1
Gustavo <andrescabral.c@gmail.com>
2017-07-17 08:20:14 -0400

    feat: Details page added

--- js/App.jsx
+++ js/App.jsx
@@ -2,9 +2,12 @@
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import React from 'react'
+import type { Match } from 'react-router-dom'
 
+import Details from './Details'
 import Landing from './Landing'
 import NotFound from './NotFound'
+import preload from '../data.json'
 import Search from './Search'
 
 const App = () => (
@@ -13,6 +16,15 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(
+              show => show.imdbID === props.match.params.id
+            )
+            return <Details show={selectedShow} {...props} />
+          }}
+        />
         <Route component={NotFound} />
       </Switch>
     </div>
