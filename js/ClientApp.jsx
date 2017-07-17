85ae24b265bc048f86b25a6cdec9b11c9579bc01
Gustavo <andrescabral.c@gmail.com>
2017-07-16 22:43:15 -0400

    build: Webpack configuration file added
    Also, ESLint ignore file added

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-const ce = React.createElement;
+const ce = React.createElement
 
 const MyTitle = props =>
-  ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  ce('div', null, ce('h1', { style: { color: props.color } }, props.title))
 
 const MyFirstComponent = () =>
   ce(
@@ -13,6 +13,6 @@ const MyFirstComponent = () =>
     ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
     ce(MyTitle, { title: 'Breaking Bad', color: 'GreenYellow' }),
     ce(MyTitle, { title: 'Orange is the New Black', color: 'LimeGreen' })
-  );
+  )
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'))
