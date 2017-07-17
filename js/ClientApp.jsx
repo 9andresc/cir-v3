e4d9d45da013d3e28530ef9ed2a1efe12b98151a
Gustavo <andrescabral.c@gmail.com>
2017-07-16 23:21:32 -0400

    refactor: JSX style applied to ClientApp

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const ce = React.createElement
+const MyTitle = props => {
+  const style = { color: props.color }
 
-const MyTitle = props =>
-  ce('div', null, ce('h1', { style: { color: props.color } }, props.title))
-
-const MyFirstComponent = () =>
-  ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Breaking Bad', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Orange is the New Black', color: 'LimeGreen' })
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
   )
+}
 
-render(ce(MyFirstComponent), document.getElementById('app'))
+const MyFirstComponent = () => (
+  <div id="my-first-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Breaking Bad" color="GreenYellow" />
+    <MyTitle title="Orange is the New Black" color="peru" />
+    <MyTitle title="Rick and Morty" color="burlywood" />
+  </div>
+)
+
+render(<MyFirstComponent />, document.getElementById('app'))
