fea6f12041cf15b8ca83152ccca60361869dee25
Gustavo <andrescabral.c@gmail.com>
2017-07-17 11:50:12 -0400

    refactor: Header component added in Search component

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,10 +3,41 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const Header = () => (
-  <header>
-    <h1><Link to="/">svideo</Link></h1>
-  </header>
-)
+const Header = (props: {
+  showSearch?: boolean,
+  handleSearchTermChange?: Function,
+  searchTerm?: string
+}) => {
+  let utilSpace
+  if (props.showSearch) {
+    utilSpace = (
+      <input
+        type="text"
+        value={props.searchTerm}
+        placeholder="Search"
+        onChange={props.handleSearchTermChange}
+      />
+    )
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">Back</Link>
+      </h2>
+    )
+  }
+
+  return (
+    <header>
+      <h1><Link to="/">svideo</Link></h1>
+      {utilSpace}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  showSearch: false,
+  handleSearchTermChange: function noop() {},
+  searchTerm: ''
+}
 
 export default Header
--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react'
 
+import Header from './Header'
 import ShowCard from './ShowCard'
 
 class Search extends Component {
@@ -20,15 +21,11 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <header>
-          <h1>svideo</h1>
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </header>
+        <Header
+          searchTerm={this.state.searchTerm}
+          showSearch
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
           {this.props.shows
             .filter(
