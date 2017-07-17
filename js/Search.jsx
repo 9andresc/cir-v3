4f540d7d013344478516f2bdc63fa34aef11eb20
Gustavo <andrescabral.c@gmail.com>
2017-07-17 09:19:40 -0400

    refactor(Share state between App and Search components):

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,13 +2,15 @@
 
 import React, { Component } from 'react'
 
-import preload from '../data.json'
-
 import ShowCard from './ShowCard'
 
 class Search extends Component {
   state = {
     searchTerm: ''
+  }
+
+  props: {
+    shows: Array<Show>
   }
 
   handleSearchTermChange = (
@@ -28,7 +30,7 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows
+          {this.props.shows
             .filter(
               show =>
                 `${show.title} ${show.description}`
--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -6,17 +6,17 @@ import Search from '../Search'
 import ShowCard from '../ShowCard'
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<Search shows={preload.shows} />)
   expect(component).toMatchSnapshot()
 })
 
 test('Search should render correct amount of shows', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<Search shows={preload.shows} />)
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
 test('Search should render correct amount of shows based on search term', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<Search shows={preload.shows} />)
   const searchWord = 'black'
   const showCount = preload.shows.filter(
     show =>
