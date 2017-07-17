a6c6101e8020d46521752b3e2c62bf96e3f48bbe
Gustavo <andrescabral.c@gmail.com>
2017-07-17 03:09:57 -0400

    refactor: Filter feature added to shows in Search page

--- js/Search.jsx
+++ js/Search.jsx
@@ -25,9 +25,14 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows.map(show => (
-            <ShowCard key={show.imdbID} show={show} />
-          ))}
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} show={show} />)}
         </div>
       </div>
     )
