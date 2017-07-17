07dd3ab8ec47c75f863725932fb84294b9ddee7f
Gustavo <andrescabral.c@gmail.com>
2017-07-17 02:55:54 -0400

    refactor: babel-plugin-transform-class-properties added
    Search component class syntax changed

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,19 +5,12 @@ import preload from '../data.json'
 import ShowCard from './ShowCard'
 
 class Search extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      searchTerm: 'this is some sort of debug statement'
-    }
-
-    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  state = {
+    searchTerm: ''
   }
 
-  handleSearchTermChange(event) {
+  handleSearchTermChange = event =>
     this.setState({ searchTerm: event.target.value })
-  }
 
   render() {
     return (
