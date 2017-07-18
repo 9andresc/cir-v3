9345c1aa8fba8fab72f2f9336fea90e09be08c79
Gustavo <andrescabral.c@gmail.com>
2017-07-17 22:11:53 -0400

    refactor: Flow syntax added to Redux files

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -6,6 +6,15 @@ declare var module: {
   }
 }
 
+declare type ActionType = 'SET_SEARCH_TERM'
+
+declare type ActionT<A: ActionType, P> = {|
+  type: A,
+  payload: P
+|}
+
+export type Action = ActionT<'SET_SEARCH_TERM', string>
+
 export type Show = {
   title: string,
   description: string,
--- js/store.js
+++ js/store.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux'
+// @flow
+
+import { createStore, compose } from 'redux'
 
 import reducers from './reducers'
 
-const store = createStore(reducers)
+const store = createStore(
+  reducers,
+  compose(
+    typeof window === 'object' &&
+      typeof window.devToolsExtension !== 'undefined'
+      ? window.devToolsExtension()
+      : f => f
+  )
+)
 
 export default store
