b5f2471bb3d1f6d418d7d00f0538cc4b63b786d7
Gustavo <andrescabral.c@gmail.com>
2017-07-17 21:09:56 -0400

    refactor: Redux reducers, actions and dispatch added to Landing component

--- js/App.jsx
+++ js/App.jsx
@@ -1,36 +1,41 @@
 // @flow
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import React from 'react'
 import type { Match } from 'react-router-dom'
+
+import preload from '../data.json'
 
 import Details from './Details'
 import Landing from './Landing'
 import NotFound from './NotFound'
-import preload from '../data.json'
 import Search from './Search'
+import store from './store'
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route
-          path="/search"
-          component={props => <Search shows={preload.shows} {...props} />}
-        />
-        <Route
-          path="/details/:id"
-          component={(props: { match: Match }) => {
-            const selectedShow = preload.shows.find(
-              show => show.imdbID === props.match.params.id
-            )
-            return <Details show={selectedShow} {...props} />
-          }}
-        />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            path="/search"
+            component={props => <Search shows={preload.shows} {...props} />}
+          />
+          <Route
+            path="/details/:id"
+            component={(props: { match: Match }) => {
+              const selectedShow = preload.shows.find(
+                show => show.imdbID === props.match.params.id
+              )
+              return <Details show={selectedShow} {...props} />
+            }}
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 )
 
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,32 @@
 // @flow
 
-import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import React from 'react'
 
-const Landing = () => (
+import { setSearchTerm } from './actions-creators'
+
+const Landing = (props: {
+  searchTerm: string,
+  handleSearchTermChange: Function
+}) => (
   <div className="landing">
     <h1>svideo</h1>
-    <input type="text" placeholder="Search" />
+    <input
+      value={props.searchTerm}
+      type="text"
+      placeholder="Search"
+      onChange={props.handleSearchTermChange}
+    />
     <Link to="/search">or Browse All</Link>
   </div>
 )
 
-export default Landing
+const mapStateToProps = state => ({ searchTerm: state.searchTerm })
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing)
